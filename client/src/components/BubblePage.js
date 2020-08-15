e9c2fc1d4efac715ae3703cfc86ef3f01283df0e
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-08-14 20:12:59 -0600

    pulling colors on page load

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +9,19 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  const getColors = () => {
+    axiosWithAuth()
+    .get('/colors')
+    .then((res) => {
+      console.log('succesfull get request from BubblePage', res)
+      setColorList(res.data)
+    })
+  }
+
+  useEffect(() => {
+    getColors()
+  }, [])
 
   return (
     <>
